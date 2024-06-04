336ea987b167b6a5d339abc8269a8bace35d5b3c
ZuyinatinK <zuyinatinkhofifah190@gmail.com>
2024-06-05 00:50:53 +0700

    code documentation home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Controller Home
+ *
+ * Controller ini berfungsi untuk menampilkan halaman utama dengan data mangrove.
+ */
 class Home extends CI_Controller {
+	/**
+     * Construct
+     *
+     * Memuat model `Mangrove_model` dan library `session`.
+     */
 	public function __construct()
     {
         parent:: __construct();
@@ -9,11 +19,21 @@ class Home extends CI_Controller {
 		$this->load->library('session');
     }
 	
+	/**
+     * Halaman Utama
+     *
+     * Menampilkan halaman utama dengan data mangrove dari database.
+     *
+     * @return void
+     */
 	public function index()
 	{
+		// Mengambil data mangrove dari model
 		$recordInformasi = $this->Mangrove_model->getDataMangrove();
+		// Menyiapkan data untuk dikirim ke view
 		$data = array('tabelbali' => $recordInformasi);
         $data['tbbali'] = $this->Mangrove_model->getAllMangrove();
+		
 		$this->load->view('home', $data);
 	}
 }
