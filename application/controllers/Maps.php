73f10cc0b685c54f3bf6880338ad1f0e27edecdd
ZuyinatinK <zuyinatinkhofifah190@gmail.com>
2024-06-05 01:30:31 +0700

    code documentation maps

--- application/controllers/Maps.php
+++ application/controllers/Maps.php
@@ -1,11 +1,22 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Controller Maps
+ *
+ * Controller ini berfungsi untuk menampilkan halaman peta dengan data mangrove.
+ */
 class Maps extends CI_Controller {
 
+	/**
+     * Construct
+     *
+     * Memuat model `Mangrove_model` dan library `session`. Juga memeriksa apakah pengguna sudah login.
+     */
 	public function __construct()
     {
         parent:: __construct();
+		// Mengecek apakah pengguna sudah login, jika belum maka akan dialihkan ke halaman login
 		if (!$this->session->userdata('email')) {
             redirect('auth');
         }
@@ -13,12 +24,22 @@ class Maps extends CI_Controller {
 		$this->load->library('session');
     }
 
+	/**
+     * Halaman Maps
+     *
+     * Menampilkan halaman maps dengan data mangrove dari database.
+     *
+     * @return void
+     */
     public function index()
 	{
+		// Mengambil data mangrove dari model
 		$recordInformasi = $this->Mangrove_model->getDataMangrove();
         
+		// Menyiapkan data untuk dikirim ke view
 		$data = array('tabelbali' => $recordInformasi);
 		$data['title'] = 'Maps - Visit Bali’s Mangrove Forest';
+		
 		$this->load->view('maps', $data);
 		
 	}
