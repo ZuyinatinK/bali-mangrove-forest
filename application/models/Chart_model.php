3293ede2d10a7ff81eb235670e2e1c67109810eb
ZuyinatinK <zuyinatinkhofifah190@gmail.com>
2024-06-05 14:08:29 +0700

    code documentation chart

--- application/models/Chart_model.php
+++ application/models/Chart_model.php
@@ -1,8 +1,21 @@
 <?php
+
+/**
+ * Chart_model
+ *
+ * Model ini digunakan untuk mengambil data terkait dengan mangrove dari database.
+ */
 class Chart_model extends CI_Model
 {   
-    // Mangrove
+    /**
+     * Mengambil jumlah data mangrove.
+     *
+     * Metode ini menghitung jumlah baris dalam tabel `tb_bali` yang menyimpan data mangrove.
+     *
+     * @return int Jumlah baris dalam tabel `tb_bali`.
+     */
     public function getJumlahMangrove(){
+        // Mengambil jumlah baris dari tabel 'tb_bali' dan mengembalikan jumlah baris tersebut
         return $this->db->get('tb_bali')->num_rows();
     }
 }
